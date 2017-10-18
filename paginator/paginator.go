8053bb87495cb70256b2e826f46c6e82d73c292c
Anton Khalikov <anton@khalikov.ru>
2017-10-18 18:59:41 +0500

    Catch beego ORM panic and proper way to handle empty Order

--- paginator/paginator.go
+++ paginator/paginator.go
@@ -1,6 +1,7 @@
 package paginator
 
 import (
+	"fmt"
 	"reflect"
 
 	"github.com/astaxie/beego/orm"
@@ -8,12 +9,22 @@ import (
 
 type copyFunc func(interface{}, interface{})
 
+func makeSlice(object interface{}) reflect.Value {
+	return reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(object)), 0, 10)
+}
+
 // GetPage loads page of objects.
-func (p *Paginator) GetPage(object interface{}, qs orm.QuerySeter, dstObj interface{}, copy copyFunc) (interface{}, int64, error) {
+func (p *Paginator) GetPage(object interface{}, qs orm.QuerySeter, dstObj interface{}, copy copyFunc) (intrf interface{}, count int64, err error) {
 	// Create a slice to begin with
-	var count int64
-	myType := reflect.TypeOf(object)
-	slice := reflect.MakeSlice(reflect.SliceOf(myType), 0, 10)
+	defer func() {
+		if e := recover(); e != nil {
+			intrf = makeSlice(dstObj).Interface()
+			err = fmt.Errorf("%v", e)
+		}
+	}()
+	slice := makeSlice(object)
+	dstType := reflect.TypeOf(dstObj)
+	dstSlice := makeSlice(dstObj)
 	// Create a pointer to a slice value and set it to the slice
 	x := reflect.New(slice.Type())
 	x.Elem().Set(slice)
@@ -23,18 +34,21 @@ func (p *Paginator) GetPage(object interface{}, qs orm.QuerySeter, dstObj interf
 			qs = qs.Filter(filterCond, filterVal)
 		}
 	}
-	_, err := qs.OrderBy(p.Order).Limit(p.Limit, p.Offset).All(x.Interface())
-	if err == nil {
-		count, err = qs.Count()
+	// is Order set?
+	if p.Order != "" {
+		qs = qs.OrderBy(p.Order)
 	}
-	dstType := reflect.TypeOf(dstObj)
-	dstSlice := reflect.MakeSlice(reflect.SliceOf(dstType), 0, 10)
-	xElem := x.Elem()
-	for i := 0; i < xElem.Len(); i++ {
-		s := xElem.Index(i)
-		d := reflect.New(dstType.Elem())
-		copy(s.Interface(), d.Interface())
-		dstSlice = reflect.Append(dstSlice, d)
+	// here we can get panic so we catch it in defer func
+	if _, err = qs.Limit(p.Limit, p.Offset).All(x.Interface()); err == nil {
+		if count, err = qs.Count(); err == nil {
+			xElem := x.Elem()
+			for i := 0; i < xElem.Len(); i++ {
+				s := xElem.Index(i)
+				d := reflect.New(dstType.Elem())
+				copy(s.Interface(), d.Interface())
+				dstSlice = reflect.Append(dstSlice, d)
+			}
+		}
 	}
 	return dstSlice.Interface(), count, err
 }
